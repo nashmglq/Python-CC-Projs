5cb343236c2821baf10a5ee402adfb00a8422309
Nash Maglaqui <nashmglq89@gmail.com>
2025-01-29 13:56:13 +0800

    gitignore and __name__

---
+++ __name__/file1.py
@@ -0,0 +1,16 @@
+def main():
+    print("Hello!")
+
+if __name__ == "__main__":
+    main()
+
+
+
+print("WOW")
+
+
+'''
+if __name__ == "__main__" is used to **only trigger the function** when the file is run directly, not when imported.
+
+If you want to trigger a function from another file, you can use `var.function()`.
+'''
---
+++ __name__/file2.py
@@ -0,0 +1,2 @@
+import file1
+print("Hi from file2!")
