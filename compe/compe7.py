db26897695e1048a564767ff6daed4e10767d5a4
Nash Maglaqui <nashmglq89@gmail.com>
2025-02-19 23:45:02 +0800

    Practicedd

---
+++ compe/compe7.py
@@ -0,0 +1,4 @@
+numbers = [10,2,19,20,110,11,3,2,1]
+numbers.sort()
+
+print(numbers[len(numbers)-1])
---
+++ lessons/introduction/joinAndsplit.py
@@ -0,0 +1,15 @@
+# Join is use to join statements
+# syntax = string.join(iterable)
+def joinFunc():
+    uses = ["h","q","HEY"]
+
+    print(" ".join(uses))
+    
+#Split or split() is to split a string to a list
+# syntax = var.split() or var.split("#") if you want to split it in certain char
+def splitFunc():
+    strs = "Hey everyone!"
+    print(strs.split())
+    
+joinFunc()
+splitFunc()
